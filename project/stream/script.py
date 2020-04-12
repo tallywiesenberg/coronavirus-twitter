940ca4c2a118644f8937e8fd9059a96cdddd56ce
Tally Wiesenberg <t.j.wies@gmail.com>
2020-04-12 14:15:00 -0400

    deployment

--- project/stream/script.py
+++ project/stream/script.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 
 import tweepy
 
-from .model import *
-from .stream.streamer import MyStreamListener
+from project.model import *
+from project.stream.streamer import MyStreamListener
 
 TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
                                    config('TWITTER_CONSUMER_SECRET'))
